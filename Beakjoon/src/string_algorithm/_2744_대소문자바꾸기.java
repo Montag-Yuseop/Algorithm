46fe99fe869414174dcf435fb9f56b2c6e2deb48
Yuseop <dbtjq1592000@gmail.com>
2023-03-10 15:09:24 +0900

    230310_ssafy

---
+++ Beakjoon/src/string_algorithm/_2744_대소문자바꾸기.java
@@ -0,0 +1,25 @@
+package string_algorithm;
+
+import java.util.Scanner;
+
+public class _2744_대소문자바꾸기 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		
+		for(int i = 0; i<s.length(); i++) {
+			char c = s.charAt(i);
+			
+			if(Character.isUpperCase(c)){
+				System.out.print(Character.toLowerCase(c));
+			} else {
+				System.out.print(Character.toUpperCase(c));
+			}
+		}
+		sc.close();
+		
+	}
+
+}
