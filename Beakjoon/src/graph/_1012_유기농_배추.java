519948f7da12729c2b6c520e7e2c7db512fde893
Yuseop <dbtjq1592000@gmail.com>
2023-03-10 13:34:00 +0900

    230310_ssafy

--- Beakjoon/src/graph/_1012_유기농_배추.java
+++ Beakjoon/src/graph/_1012_유기농_배추.java
@@ -3,7 +3,6 @@ package graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
