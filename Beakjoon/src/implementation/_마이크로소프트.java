7fbfcc66dc1959f401dd793bf39d1e517813c1d4
Yuseop <dbtjq1592000@gmail.com>
2023-03-15 17:01:18 +0900

    ssafy

---
+++ Beakjoon/src/implementation/_마이크로소프트.java
@@ -0,0 +1,12 @@
+package implementation;
+
+public class _마이크로소프트 {
+	public static void main(String[] args) {
+	       System.out.println("       _.-;;-._");
+	       System.out.println("'-..-'|   ||   |");
+	       System.out.println("'-..-'|_.-;;-._|");
+	       System.out.println("'-..-'|   ||   |");
+	       System.out.println("'-..-'|_.-''-._|");
+	}
+
+}
